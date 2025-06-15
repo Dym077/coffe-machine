ddd4f7c63464d7adb41bb9a26a9b1eccaa91bf8b
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2025-06-15 19:43:42 +0200

    Add show available recipes

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+def get_available_drinks(menu, resources):
+    """Filters the menu to only return available drinks"""
+    available_drinks = []
+    for drink in menu.menu:
+        can_make = True
+        for item in drink.ingredients:
+            if drink.ingredients[item] > resources[item]:
+                can_make = False
+                break
+        if can_make:
+            available_drinks.append(drink)
+    return available_drinks
+
 
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
@@ -10,7 +23,18 @@ menu = Menu()
 machine_on = True
 
 while machine_on:
+    available_drinks = get_available_drinks(menu, coffee_maker.resources)
+
+    if available_drinks:
+        print("Available drinks:")
+        for available_drink in available_drinks:
+            print(f"- {available_drink.name} (${available_drink.cost})")
+    else:
+        print("We're sorry We can't make anything right now.")
+        break
+
     user_input = input("What would you like? (espresso/latte/cappuccino/report/off): ").lower()
+
     # TODO 1: Print report
     if user_input == "report":
         coffee_maker.report()
@@ -21,16 +45,15 @@ while machine_on:
     else:
         drink = menu.find_drink(user_input)
         if drink:
-            # TODO 6: Customize drink
-            extra_water = int(input("Extra water (ml)? (0 for none):"))
-            extra_milk = int(input("Extra milk (ml)? (0 for none):"))
-            extra_coffee = int(input("Extra coffee (ml)? (0 for none):"))
+            if drink in available_drinks:
+                # TODO 6: Customize drink
+                extra_water = int(input("Extra water (ml)? (0 for none):"))
+                extra_milk = int(input("Extra milk (ml)? (0 for none):"))
+                extra_coffee = int(input("Extra coffee (ml)? (0 for none):"))
 
-            drink.customize(extra_water, extra_milk, extra_coffee)
+                drink.customize(extra_water, extra_milk, extra_coffee)
 
 
-            # TODO 2: Check Resources sufficient?
-            if coffee_maker.is_resource_sufficient(drink):
                 # TODO 3: Process coins
                 # TODO 4: Check transaction successful
                 if money_machine.make_payment(drink.cost):
